b9a8cf961287429159ca9aab81e5069a51b51812
Tyler Antoni <79325792+Dibble710@users.noreply.github.com>
2021-10-29 22:32:29 -0400

    Added scroll-in transitions

--- script.js
+++ script.js
@@ -44,14 +44,22 @@ $(document).ready(function () {
     loop: true,
   });
   var typed = new Typed(".typing-2", {
-    strings: [
-      "Music",
-      "Computers",
-      "Video Games",
-      "Coding!",
-    ],
+    strings: ["Music", "Computers", "Video Games", "Coding!"],
     typeSpeed: 100,
     backSpeed: 50,
     loop: true,
   });
 });
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 1500,
+  delay: 200,
+});
+
+ScrollReveal().reveal('.about-content, .skills-content,.contact-content, .home-content', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.serv-content .card', { delay: 600, origin: 'bottom' });
+ScrollReveal().reveal('.bars', { delay: 200, origin: 'right' });
+ScrollReveal().reveal('.title', { delay: 200, origin: 'top' });
+ScrollReveal().reveal('#icon-code', { delay: 200, origin: 'left', interval: 200 });
